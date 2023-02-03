b0ff72ad972d1a6e786861286ae4296044f8dd62
sotalbireo <athene609.cyl@gmail.com>
2023-02-03 10:07:33 +0900

    2.2

--- build.js
+++ build.js
@@ -4,7 +4,7 @@ const shell = require('shelljs')
 
 shell.ls('./')
 
-shell.exec('git clone -q --depth=1 https://${GITHUB_USER}:${GITHUB_TOKEN}@github.com/${GITHUB_REPOSITORY} data')
+shell.exec('git clone -q --depth=1 https://${GITHUB_USER}:${GITHUB_TOKEN}@github.com/${GITHUB_USER}/${GITHUB_REPOSITORY}.git data')
 shell.exec('nuxt build')
 shell.mkdir('-p', '.vercel/output/functions/index.func')
 shell.cp('-r', 'data', '.vercel/output/functions/index.func')
